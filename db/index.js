38259a64702f90bc4efc622911ba03f5a0f62f85
bjarte <22171847+freebattie@users.noreply.github.com>
2023-05-28 09:24:43 +0200

    first commit

---
+++ db/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import pg from "pg";
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+  ssl: SSL_RULE,
+});
+
+const query = (text, params) => pool.query(text, params);
+
+export { query, createUpdateObjSQL, rollBackQuery };
