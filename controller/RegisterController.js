a2c0bae9e33114cf7d865dd84e4b9a2d5505744f
Michaël Mendes <30933848+Mimea005@users.noreply.github.com>
2023-05-31 13:10:09 +0200

    Added casual registration controller for /api/register

--- controller/RegisterController.js
+++ controller/RegisterController.js
@@ -2,14 +2,22 @@ import * as Services from "../services/CreateService.js"
 
 export async function register(req, res) {
   // Verify username
-  let { wanted_name: wanted_name, pass } = req.body;
+  const { userName, password } = req.body;
+  if (userName === "" || password === "") {
+    return res.sendStatus(403);
+  }
 
-  if (Services.userExists(wanted_name)) {
+  let exists = await Services.userExists(userName);
+
+  console.log("User does " + (exists ? "" : "not ") + "exist");
+
+  if (exists) {
     res.status(409);
     res.send("User already exists");
   }
   // Create user
-  else if (Services.addUser(wanted_name, pass).await == true) {
+  // Role is hardcoded because the registration form is only for normal users
+  else if (await Services.addUser(userName, password, "casual") == true) {
     res.sendStatus(200);
   }
 
