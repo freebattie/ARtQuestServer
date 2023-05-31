797c4b9beb570b14ed38dbcff4821e54b14085b7
Michaël Mendes <30933848+Mimea005@users.noreply.github.com>
2023-05-31 10:48:21 +0200

    Started db implementation

--- controller/RegisterController.js
+++ controller/RegisterController.js
@@ -2,12 +2,14 @@ import * as Services from "../services/CreateService.js"
 
 export async function register(req, res) {
   // Verify username
-  if (Services.userExists()) {
+  let { wanted_name: wanted_name, pass } = req.body;
+
+  if (Services.userExists(wanted_name)) {
     res.status(409);
     res.send("User already exists");
   }
   // Create user
-  else if (Services.addUser().await == true) {
+  else if (Services.addUser(wanted_name, pass).await == true) {
     res.sendStatus(200);
   }
 
--- services/CreateService.js
+++ services/CreateService.js
@@ -1,15 +1,14 @@
-import db from "../db/index.js";
+import * as db from "../db/index.js";
 
 export async function userExists(username) {
-  // TODO: Implementation requires the db tables to be setup
-  console.error("CreateService.userExists is NOT implemented!");
-  return false;
+   let result = await db.query("SELECT username from users where username = $1", [username]);
+   return result.len > 0;
 }
 
 /// Returns false if an error occured during user creation and loggs error
 export async function addUser(username, password) {
-  // TODO: Implementation requires the db tables to be setup
-  console.error("CreateService.addUser is NOT implemented!");
-  return false;
+   // TODO: Implementation requires the db tables to be setup
+   console.error("CreateService.addUser is NOT implemented!");
+   return false;
 }
 
