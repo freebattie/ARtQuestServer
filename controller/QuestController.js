bc49b824b2b702a1d6f06a5f853cbe5cf7183c13
Michaël Mendes <30933848+Mimea005@users.noreply.github.com>
2023-06-01 14:43:21 +0200

    Added function signatures

---
+++ controller/QuestController.js
@@ -0,0 +1,10 @@
+
+export async function getAllQuests() {
+   console.log("getAllQuests()");
+   return [];
+}
+
+export async function updateQuest(item) {
+   console.log("updateQuest()");
+   return {};
+}
--- routes.js
+++ routes.js
@@ -1,23 +1,34 @@
 const TEST = "/api/test";
 const LOGINPATH = "/api/login";
 const REGISTERPATH = "/api/register";
+const QUESTAPI = "/api/quest";
 
 import * as TestController from "./controller/TestController.js";
 import * as LoginController from "./controller/LoginController.js";
 import * as RegisterController from "./controller/RegisterController.js";
+import * as QuestController from "./controller/QuestController.js";
 
 const routes = (app) => {
   //LOGIN ROUTES
   app.post(LOGINPATH, LoginController.login);
   app.post(REGISTERPATH, RegisterController.register);
 
-    //TEST ROUTES
-    // rute som krever at bruker er logget inn
-    app.get(
-      TEST,
-      LoginController.requestUser,
-      TestController.getAllUsersFromTestTable
-    );
+  // Quest routes
+  app.route(QUESTAPI)
+    .all(LoginController.requestUser)
+    .get(QuestController.getAllQuests)
+    .post(QuestController.updateQuest)
+    ;
+
+
+  //TEST ROUTES
+  // rute som krever at bruker er logget inn
+  app.get(
+    TEST,
+    LoginController.requestUser,
+    TestController.getAllUsersFromTestTable
+  );
+
   //rute som ikke krever at bruker er logget inn
   app.get(TEST + "/:id", TestController.getUserFromTestTable);
 };
