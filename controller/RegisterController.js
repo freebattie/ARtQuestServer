26f6c607a4b61d3127a49f9e66bbd7e862e0eaf9
bjarte <22171847+freebattie@users.noreply.github.com>
2023-06-01 22:29:41 +0200

    fix  create user

--- controller/RegisterController.js
+++ controller/RegisterController.js
@@ -1,26 +1,25 @@
-import * as Services from "../services/CreateService.js"
+import * as Services from "../services/CreateService.js";
 
 export async function register(req, res) {
-    // Verify username
-    const {userName, password} = req.body;
-    if (userName === "" || password === "") {
-        return res.sendStatus(403);
-    }
+  // Verify username
+  const { email, password } = req.body;
+  if (email === "" || password === "") {
+    return res.sendStatus(403);
+  }
 
-    let exists = await Services.userExists(userName);
+  let exists = await Services.userExists(email);
 
-    console.log("User does " + (exists ? "" : "not ") + "exist");
+  console.log("User does " + (exists ? "" : "not ") + "exist");
 
-    if (exists) {
-        res.status(409);
-        res.send("User already exists");
-    }
-        // Create user
-    // Role is hardcoded because the registration form is only for normal users
-    else if (await Services.addUser(userName, password, "casual") === true) {
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(500);
-    }
-
+  if (exists) {
+    res.status(409);
+    res.send("User already exists");
+  }
+  // Create user
+  // Role is hardcoded because the registration form is only for normal users
+  else if ((await Services.addUser(email, password, "casual")) === true) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(500);
+  }
 }
