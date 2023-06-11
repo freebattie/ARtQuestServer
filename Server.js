a05770b0aade77c26373e66f99a27cd0711ac6d7
bjarte <22171847+freebattie@users.noreply.github.com>
2023-06-11 14:05:19 +0200

    added nodemailer and a promo mail

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 
 const __FILENAME = fileURLToPath(import.meta.url);
 const __DIRNAME = path.dirname(__FILENAME);
@@ -21,16 +21,17 @@ import routes from "./Routes.js";
 import http from "http";
 import bodyParser from "body-parser";
 import cookies from "cookie-parser";
+import { sendPromoMail } from "./lib/mail.js";
 
 const APP = express();
-APP.use(bodyParser.json({type: "*/*"}));
-
+APP.use(bodyParser.json({ type: "*/*" }));
+sendPromoMail("bjartenerland5@hotmail.com");
 //APP.use(express.static(path.join(__DIRNAME, "/public"))); If we want to sent img etc from server
 APP.use(cookies(process.env.COOKIE_SECRET));
 APP.use(async (req, res, next) => {
-    console.log(`${req.ip}: ${req.method} ${req.url}`);
-    next();
-    console.log(`${req.ip}: ${res.statusCode}`);
+  console.log(`${req.ip}: ${req.method} ${req.url}`);
+  next();
+  console.log(`${req.ip}: ${res.statusCode}`);
 });
 routes(APP);
 
